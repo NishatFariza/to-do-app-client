e5dee04a6cd63a492c0bc95c97cead4e97251904
NishatFariza <nishatfarzia78@gmail.com>
2022-05-19 03:12:16 +0600

    axios post api

--- src/Pages/AddToDo/AddToDo.jsx
+++ src/Pages/AddToDo/AddToDo.jsx
@@ -1,10 +1,46 @@
+import axios from "axios";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading/Loading";
 
 const AddToDo = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const description = event.target.description.value;
+    // console.log(name, description);
+    const newTask = {
+      name: name,
+      description: description,
+    };
+
+    const url = `https://sheltered-sands-62975.herokuapp.com/`;
+
+    axios
+      .post(url, newTask)
+      .then(function (response) {
+        console.log(response);
+        if (response.data.insertedId) {
+          toast.success("Task Added Successfully");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div className="lg:w-9/12 w-11/12 mx-auto mt-16 mb-20">
-      <div className="my-10 lg:w-4/12 w-11/12 mx-auto flex items-center justify-center shadow-lg rounded-md">
-        <div>
+      <div className="my-10 lg:w-4/12 w-11/12 mx-auto flex items-center justify-center shadow-lg rounded-md py-6">
+        <form onSubmit={handleAddTask}>
           <div class="form-control w-full max-w-xs">
             <label class="label">
               <span class="label-text font-bold">Task Name</span>
@@ -12,6 +48,7 @@ const AddToDo = () => {
             <input
               type="text"
               placeholder="Task Name"
+              name="name"
               class="input input-bordered w-full max-w-xs"
             />
           </div>
@@ -22,6 +59,7 @@ const AddToDo = () => {
             <textarea
               class="textarea textarea-bordered"
               placeholder="Description"
+              name="description"
             ></textarea>
           </div>
           <input
@@ -29,13 +67,12 @@ const AddToDo = () => {
             type="submit"
             value="Add Task"
           />
-        </div>
+        </form>
       </div>
       <div class="overflow-x-auto">
         <table class="table w-full">
           <thead>
             <tr>
-              <th></th>
               <th>Name</th>
               <th>description</th>
               <th>Delete</th>
@@ -43,24 +80,116 @@ const AddToDo = () => {
           </thead>
           <tbody>
             <tr>
-              <th>1</th>
               <td>Cy Ganderton</td>
               <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-
-            <tr class="hover">
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
+              <td>
+                <button class="btn btn-square btn-outline">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
             </tr>
 
             <tr>
-              <th>3</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>
+                <button class="btn btn-square btn-outline">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+
+            <tr>
               <td>Brice Swyre</td>
               <td>Tax Accountant</td>
-              <td>Red</td>
+              <td>
+                <button class="btn btn-square btn-outline">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>
+                <button class="btn btn-square btn-outline">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>
+                <button class="btn btn-square btn-outline">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
